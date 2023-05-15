6d762d297d5fec7e9b181fa6f28231f328eba31b
彭康楨 (james_peng) <sis3970@sis.com.tw>
2023-05-15 15:44:25 +0800

    updata actions

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def getOfiiiM3u8(id):
 
 def main():
     out = '#EXTM3U url-tvg="http://epg.51zmt.top:8000/e.xml"\n'
-    txt = 'OfiiiTV,#genre#\n'
+    txt = '歐飛頻道,#genre#\n'
     for a in channels:
         title = a['title']
         id = a['id']
@@ -131,7 +131,33 @@ def main():
         f.write(f'{m3u8}\n')
         f.close()
 
+    txt += '民視頻道,#genre#\n'
+    ftvchannels = [['FTV', '01', '民视'],['FMTV', '02', '民视第一台'],['FTTV', '03', '民视台湾台'],['FTVNews', '04', '民视新闻台'],['FTVDrama', '05', '民視影劇台'],['FTVVariety', '06', '民视综艺台'],['FTVTravel', '07', '民视旅游台']]
+    for ch in ftvchannels:
+        v = ch[0]
+        id = ch[1]
+        title = ch[2]
 
+        url = "https://app.4gtv.tv/Data/GetChannelURL_Mozai.ashx?Channelname="+v
+        try:
+            resp = requests.get(url, headers=HEADERS)
+        except Exception as e:
+            print(e)
+        data = resp.text
+        x = data.replace("channelname(", "").replace(")", "")
+        j = json.loads(x)
+        link = j['VideoURL'].replace("master.m3u8", "stream2.m3u8")
+        image = "https://www.ftv.com.tw/images/Ch_"+id+".png"
+
+        out += '#EXTINF:-1 tvg-name="'+title+'" tvg-logo="'+image + '" group-title="民視頻道HD",'+title+'\n'+link + '\n'
+        txt += title + ','+ link + '\n'
+
+        outFile = title+'.m3u8'
+        f = open(outFile,'w',encoding='utf-8')
+        f.write(f'#EXTM3U\n')
+        f.write(f'#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2048000,RESOLUTION=1920x1080\n')
+        f.write(f'{link}\n')
+        f.close()
 
     with open("tv.m3u",'w',encoding='utf-8') as f:
         f.write(out)
@@ -144,4 +170,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
