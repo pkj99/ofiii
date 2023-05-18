1069dcdfaac2dcde7691e62ef73e5284d60acb91
彭康楨 (james_peng) <sis3970@sis.com.tw>
2023-05-18 10:37:29 +0800

    updata actions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import bs4
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"}
 
